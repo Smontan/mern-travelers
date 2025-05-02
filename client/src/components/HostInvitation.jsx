15f5b4cf5fb01b673f2f92e27c95947ede07c2b3
Sherwin <94516340+Smontan@users.noreply.github.com>
2025-05-02 21:33:44 +0900

    Created host invitation secton

--- client/src/components/HostInvitation.jsx
+++ client/src/components/HostInvitation.jsx
@@ -1,6 +1,14 @@
 const HostInvitation = () => {
   return (
-    <div>HostInvitation</div>
-  )
-}
-export default HostInvitation
+    <section className="container mx-auto">
+      <div className=" rounded-lg bg-primary mx-5 lg:mx-0 py-15 ps-5 lg:ps-10">
+        <h3 className="max-w-[300px] text-4xl font-bold text-light-text leading-12 wrap-break-word ">
+          Try Hosting With Us
+        </h3>
+        <p className="mt-2 text-[16px] text-light-text">Earn extra just by renting your property.</p>
+        <button className="py-5 px-10 mt-8 rounded-full bg-secondary text-center text-sm font-bold shadow-lg">Become a host</button>
+      </div>
+    </section>
+  );
+};
+export default HostInvitation;
