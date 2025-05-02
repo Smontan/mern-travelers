edcc40f425b7e9d5f504697e99eb36cda4d32af5
Sherwin <94516340+Smontan@users.noreply.github.com>
2025-05-02 20:33:09 +0900

    Created nearby properties component

--- client/src/components/NearbyProperties.jsx
+++ client/src/components/NearbyProperties.jsx
@@ -1,6 +1,53 @@
+import bgImg from "../assets/hotels.avif";
+import hostImg from "../assets/owner.png";
+import PropertyCard from "./cards/PropertyCard";
+import Title from "./Title";
+
 const NearbyProperties = () => {
+  const latestProperties = [
+    {
+      title: "Well Furnished Appartment",
+      address: "100 Smart street, LA, USA",
+      cardImg: bgImg,
+      ownerImg: hostImg,
+    },
+    {
+      title: "Well Furnished Appartment",
+      address: "100 Smart street, LA, USA",
+      cardImg: bgImg,
+      ownerImg: hostImg,
+    },
+    {
+      title: "Well Furnished Appartment",
+      address: "100 Smart street, LA, USA",
+      cardImg: bgImg,
+      ownerImg: hostImg,
+    },
+    {
+      title: "Well Furnished Appartment",
+      address: "100 Smart street, LA, USA",
+      cardImg: bgImg,
+      ownerImg: hostImg,
+    },
+  ];
   return (
-    <div>NearbyProperties</div>
-  )
-}
-export default NearbyProperties
+    <section className="container mx-auto overflow-x-auto">
+      <Title>Nearby listed Properties</Title>
+      {/* Latest Property cards */}
+      <div className="flex mt-16 gap-7.5  mx-5 lg:mx-0 pb-8">
+        {latestProperties?.map(
+          ({ title, address, cardImg, ownerImg }, index) => (
+            <PropertyCard
+              title={title}
+              address={address}
+              ownerImg={ownerImg}
+              cardImg={cardImg}
+              key={index}
+            />
+          )
+        )}
+      </div>
+    </section>
+  );
+};
+export default NearbyProperties;
