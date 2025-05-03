4163cbd853aeddabab2a2848c9b455801b3b4cf6
Sherwin <94516340+Smontan@users.noreply.github.com>
2025-05-03 16:17:45 +0900

    Updte the props of the logo and put some customize colors in logo

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,14 +1,14 @@
 import { Luggage } from "lucide-react";
 import { Link } from "react-router";
 
-const Logo = () => {
+const Logo = ({ color }) => {
   return (
     <Link
       to={"/"}
-      className="inline-flex gap-1 items-center text-2xl font-extrabold "
+      className={`inline-flex gap-1 items-center text-2xl font-extrabold`}
     >
-      <Luggage width={32} height={32} />
-      <span className="tracking-tighter">Travelers</span>
+      <Luggage width={32} height={32} className={color} />
+      <span className={`tracking-tighter ${color}`}>Travelers</span>
     </Link>
   );
 };
