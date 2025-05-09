ff24f6350e4f260e56dbfdc03cc9c1fffa86193e
Sherwin <94516340+Smontan@users.noreply.github.com>
2025-05-09 18:26:07 +0900

    Add an observer for title bottom border animation

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+
 const Title = ({ children }) => {
+  const titleRef = useRef();
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsActive(true);
+          console.log("observer is intersecting: " + entry.isIntersecting);
+          // observer.disconnect();
+        } else setIsActive(false); // Optional : reset when it leaves the viewport
+      },
+      {
+        threshold: .1, // Adjust the value is needed
+      }
+    );
+
+    if (titleRef.current) observer.observe(titleRef.current);
+
+    return () => {
+      if (titleRef.current) observer.unobserve(titleRef.current);
+    };
+  }, []);
+
   return (
-    <div className="w-fit">
-      <h2 className="title-underline-border max-w-[400px] ms-5 lg:ms-0 text-4xl font-bold leading-12 wrap-break-word">
+    <div className="w-fit" ref={titleRef}>
+       <h2
+        className={`title-underline-border max-w-[400px] ms-5 lg:ms-0 text-4xl font-bold leading-12 wrap-break-word ${
+          isActive ? " active" : ""
+        }`}
+      >
         {children}
       </h2>
     </div>
